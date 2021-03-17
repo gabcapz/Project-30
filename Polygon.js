da40fb0ab6a482745218793980a0ad82ca1b4c17
gabcapz <75875376+gabcapz@users.noreply.github.com>
2021-03-17 13:50:23 +0530

    Add files via upload

---
+++ Polygon.js
@@ -0,0 +1,24 @@
+class Polygon{
+    constructor(x,y,r){
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0,
+            'isStatic' : true
+        }
+        this.body = Bodies.circle(50, 200, 20);
+        this.r = r;
+        this.image = loadImage("polygon.png");
+        World.add(world,this.body);
+    }
+
+    display(){
+        push();
+        var angle = this.body.angle;
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image ,this.body.position.x,this.body.position.y,40,40);
+        pop();
+    }
+}
